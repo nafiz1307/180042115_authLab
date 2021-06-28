acd1775326324b5866bffc565541339c89db1a92
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-06-28 09:32:44 +0600

    validation including password match works

--- routes/auth.js
+++ routes/auth.js
@@ -1,44 +1,37 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const bcrypt = require('bcryptjs');
+const {loginValidation, registrationValidation}= require('./validation');
 
 
 
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema ={
-    name : Joi.string().min(6).required(),
-    email : Joi.string().required().email(),
-    password: Joi.string().min(6).required(),
-    gender: Joi.string().valid("male", "female").error(() => 'Gender should be Male (or) Female')
-}
 
 router.post('/register', async (req,res)=>{
 
-    //Data Validation before user input
-    const {error} = Joi.validate(req.body,schema);
-    // res.send(error.details[0].message);
+    // //Data Validation before user input
+    // const {error} = Joi.validate(req.body,schema);
+    // // res.send(error.details[0].message);
+    const {error}= registrationValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
     
-    //Hashing Passwords
-    // const salt = await bcrypt.genSalt(10);
-    // const hashedPassword = await bcrypt.hash(req.body.password,salt);
+    // Hashing Passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password,salt);
 
     
-    // //Create a new User
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: hashedPassword,
-    //     gender: req.body.gender
-    // });
-    // try{
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // }catch(err){
-    //     console.error(err.message)
-    // }
+    //Create a new User
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        gender: req.body.gender
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        console.error(err.message)
+    }
 });
 
 //Login Route
---
+++ routes/validation.js
@@ -0,0 +1,30 @@
+//Validation
+const Joi = require("@hapi/joi");
+
+//Registration Validation
+const registrationValidation = (data) => {
+  const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().min(6).required(),
+    password_confirmation: Joi.any()
+      .valid(Joi.ref("password"))
+      .required()
+      .options({ language: { any: { allowOnly: "must match password" } } }),
+    gender: Joi.string()
+      .valid("male", "female")
+      .error(() => "Gender should be Male (or) Female"),
+  };
+  return Joi.validate(data, schema);
+};
+
+const loginValidation = () => {
+  const schema = {
+    email: Joi.string().required().email(),
+    password: Joi.string().min(6).required(),
+  };
+  return Joi.validate(data, schema);
+};
+
+module.exports.registrationValidation = registrationValidation;
+module.exports.loginValidation = loginValidation;
